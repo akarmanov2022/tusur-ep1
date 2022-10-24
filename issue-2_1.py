c21d061c34a0b144ed3a75ae9166a122d007d855
akarmanoff <arseniy.karmanov@mail.ru>
2022-10-24 21:40:58 +0700

    Выполнил задание 2.1.

---
+++ issue-2_1.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+A = [[-4, -1, -1, 6],
+     [4, 8, 8, 0],
+     [-2, -1, 8, -2],
+     [6, 1, 8, 0]]
+B = [-36, -8, -19, -11]
+
+detA = np.linalg.det(A)
+print(detA)
+
+
+def get_sub_det(matrix_a, matrix_b, index: int):
+    temp = list()
+    for i in range(len(matrix_a)):
+        copy_row = list(matrix_a[i])
+        copy_row[index] = matrix_b[i]
+        temp.append(copy_row)
+    print(temp)
+    return np.linalg.det(temp)
+
+
+detA1 = get_sub_det(A, B, 0)
+print(detA1)
+detA2 = get_sub_det(A, B, 1)
+print(detA1)
+detA3 = get_sub_det(A, B, 2)
+print(detA1)
+detA4 = get_sub_det(A, B, 3)
+print(detA1)
+print(A)
+X = [
+    detA1 / detA,
+    detA2 / detA,
+    detA3 / detA,
+    detA4 / detA
+]
+
+print(X)
